26b182ca31af5017fe34df4a44a3f252292e394e
Daniel <daniel-erik.kiuru@voco.ee>
2024-11-11 10:44:04 +0200

    Practice 1

---
+++ pr1.ps1
@@ -0,0 +1,11 @@
+﻿# 2 arrayd
+$a1 = @(1,2,3)
+$a2 = @(4,5,6)
+# tühi array
+$a3 = @()
+# lisab uued väärtused arraysse
+$a3 += $a1[0] + $a2[0]
+$a3 += $a1[1] + $a2[1]
+$a3 += $a1[2] + $a2[2]
+# prindib kolmanda array
+$loend3
