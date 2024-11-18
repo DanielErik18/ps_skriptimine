edb87acaa3f240614d2c5c6ad10bb7d0e4fc50ef
Daniel <daniel-erik.kiuru@voco.ee>
2024-11-18 08:41:07 +0200

    practice 4.1

---
+++ pr4_1.ps1
@@ -0,0 +1,11 @@
+﻿$arv1 = Read-Host "Sisesta esimene arv: "
+$arv2 = Read-Host "Sisesta teine arv: "
+
+if([int]$arv1 -gt [int]$arv2)
+    {
+    Write-Host "Esimene arv $arv1 on suurem kui teine arv $arv2"
+    }
+else
+    {
+    Write-Host "Teine arv $arv2 on suurem, kui esimene arv $arv1"
+    }
