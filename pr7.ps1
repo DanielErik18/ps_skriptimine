a61fb58bd1ec0358cc24f24471209b8b1ac7a493
Daniel <daniel-erik.kiuru@voco.ee>
2024-11-18 10:13:43 +0200

    Practice 7

---
+++ pr7.ps1
@@ -0,0 +1,14 @@
+﻿$group = @("Red","Yellow","Blue","Green")
+
+$result=@()
+
+for($i=1 ; $i -le 20 ; $i++)
+    {
+    $g = Get-Random $group
+    $temp = [PSCustomObject]@{
+    RollNumber = $i
+    Group = $g
+    }
+    $result += $temp
+    }
+$result
