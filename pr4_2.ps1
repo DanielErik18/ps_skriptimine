d1695a63c22d9db80406a750c00646aa98ebef28
Daniel <daniel-erik.kiuru@voco.ee>
2024-11-18 09:07:59 +0200

    Practice 4.2

---
+++ pr4_2.ps1
@@ -0,0 +1,21 @@
+﻿Write-Host "Select country: " -ForegroundColor Yellow
+Write-Host "1: India `n2: Australia `n3: China" -ForegroundColor Cyan
+
+$number= Read-Host "Please select country"
+
+if($number -eq 1)
+    {
+    Write-Host "India's capital is New Delhi" -ForegroundColor Green
+    }
+elseif($number -eq 2)
+    {
+    Write-Host "Australia's capital is Canberra" -ForegroundColor Green
+    }
+elseif($number -eq 3)
+    {
+    Write-Host "China's capital is Bejing" -ForegroundColor Green
+    }
+else
+    {
+    Write-Host "Please select country from list" -ForegroundColor Red
+    }
