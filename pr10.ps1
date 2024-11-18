c8fe4b511d3aa0376224ae0161f50f68d82af9da
Daniel <daniel-erik.kiuru@voco.ee>
2024-11-18 10:38:13 +0200

    Practice 10

---
+++ pr10.ps1
@@ -0,0 +1,11 @@
+﻿$count = 0
+
+do
+{
+    Write-Host "Notepad is running"
+    $count ++
+    Start-Sleep -Seconds 1
+    $isNotepadOpen = Get-Process notepad -ea SilentlyContinue
+}until($isNotepadOpen -eq $null )
+
+$count
