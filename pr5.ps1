8ff40f7d3bc9e0cc99227b0404b68e50225b34b2
Daniel <daniel-erik.kiuru@voco.ee>
2024-11-18 11:42:00 +0200

    Practice 5 i ja ii

---
+++ pr5.ps1
@@ -0,0 +1,11 @@
+﻿#Esimene i osa
+#Get-Process | where{$_.ProcessName -eq "notepad"} | Select ProcessName, Id
+
+#Teine ii osa
+Get-ChildItem C:\temp\test
+
+$file = Get-ChildItem C:\temp\test | where{$_.Name -like "*.csv"} | select Name, Length
+$sizeKB = $file.Length/1KB
+$sizeMB = $file.Length/1MB
+
+Write-Host "`nFilename : " $file.Name "`nSize in KB : " $sizeKB "`nSize in MB : " $sizeMB 
