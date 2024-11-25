c72e4e503d5217cd479d6264dde6bc4fe4f066ea
Daniel <daniel-erik.kiuru@voco.ee>
2024-11-25 09:12:23 +0200

    Practice 12.1

---
+++ pr12_1.ps1
@@ -0,0 +1,15 @@
+﻿function countstart
+{
+$running = Get-Service | ?{$_.Status -eq "Running"}
+Write-Host "Total services in running state = " $running.count
+}
+
+countstart
+
+function countstop
+{
+$stopped = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-Host "Total services in running state = " $stopped.count
+}
+
+countstop
